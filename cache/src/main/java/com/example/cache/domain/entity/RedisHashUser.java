80ac63fa3b02c5d2fc09f7813fd3f000d4ccd170
jason07289 <jason07999@naver.com>
2024-10-22 14:37:22 +0900

    redisTemplate, cache 활용

--- cache/src/main/java/com/example/cache/domain/entity/RedisHashUser.java
+++ cache/src/main/java/com/example/cache/domain/entity/RedisHashUser.java
@@ -1,10 +1,8 @@
 package com.example.cache.domain.entity;
 
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
